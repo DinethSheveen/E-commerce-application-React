21a92fc6601e99d233e91b3130d6eaa9ad4c0262
DinethSheveen <fdineth4000@gmail.com>
2025-09-23 13:31:42 +0100

    Added react routing to the tracking and the orders page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Homepage from './pages/Homepage'
 import {Routes, Route} from 'react-router'
 import Checkout from './pages/Checkout'
+import Orders from './pages/Orders'
+import Tracking from './pages/Tracking'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route path='/' element={<Homepage/>}/>
       <Route path='checkout' element={<Checkout/>}/>
+      <Route path='orders' element={<Orders/>}/>
+      <Route path='tracking' element={<Tracking/>}/>
     </Routes>
     </>
   )

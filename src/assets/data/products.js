32b85c062b22d427b33a90607cf70209fe9692b3
DinethSheveen <fdineth4000@gmail.com>
2025-09-23 17:31:41 +0100

    file consisting product data

---
+++ src/assets/data/products.js
@@ -0,0 +1,464 @@
+export const products = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    productImage: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    productName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    productRating: {
+      stars: 4.5,
+      count: 87
+    },
+    productPrice: 1090,
+    keywords: ["socks", "sports", "apparel"]
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    productImage: "images/products/intermediate-composite-basketball.jpg",
+    productName: "Intermediate Size Basketball",
+    productRating: {
+      stars: 4,
+      count: 127
+    },
+    productPrice: 2095,
+    keywords: ["sports", "basketballs"]
+  },
+  {
+    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    productImage: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    productName: "Adults Plain Cotton T-Shirt - 2 Pack",
+    productRating: {
+      stars: 4.5,
+      count: 56
+    },
+    productPrice: 799,
+    keywords: ["tshirts", "apparel", "mens"]
+  },
+  {
+    productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    productImage: "images/products/2-slot-toaster-white.jpg",
+    productName: "2 Slot Toaster - White",
+    productRating: {
+      stars: 5,
+      count: 2197
+    },
+    productPrice: 1899,
+    keywords: ["toaster", "kitchen", "appliances"]
+  },
+  {
+    idproductId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    productImage: "images/products/elegant-white-dinner-plate-set.jpg",
+    productName: "2 Piece White Dinner Plate Set",
+    productRating: {
+      stars: 4,
+      count: 37
+    },
+    productPrice: 2067,
+    keywords: ["plates", "kitchen", "dining"]
+  },
+  {
+    productId: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    productImage: "images/products/3-piece-cooking-set.jpg",
+    productName: "3 Piece Non-Stick, Black Cooking Pot Set",
+    productRating: {
+      stars: 4.5,
+      count: 175
+    },
+    productPrice: 3499,
+    keywords: ["kitchen", "cookware"]
+  },
+  {
+    productId: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    productImage: "images/products/women-plain-cotton-oversized-sweater-gray.jpg",
+    productName: "Cotton Oversized Sweater - Gray",
+    productRating: {
+      stars: 4.5,
+      count: 317
+    },
+    productPrice: 2400,
+    keywords: ["sweaters", "apparel"]
+  },
+  {
+    productId: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    productImage: "images/products/luxury-towel-set.jpg",
+    productName: "2 Piece Luxury Towel Set - White",
+    productRating: {
+      stars: 4.5,
+      count: 144
+    },
+    productPrice: 3599,
+    keywords: ["bathroom", "washroom", "restroom", "towels", "bath towels"]
+  },
+  {
+    productId: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    productImage: "images/products/facial-tissue-2-ply-8-boxes.jpg",
+    productName: "Ultra Soft Tissue 2-Ply - 8 Boxes",
+    productRating: {
+      stars: 4,
+      count: 99
+    },
+    productPrice: 2374,
+    keywords: ["kleenex", "tissues", "kitchen", "napkins"]
+  },
+  {
+    productId: "5968897c-4d27-4872-89f6-5bcb052746d7",
+    productImage: "images/products/women-striped-beach-dress.jpg",
+    productName: "Women's Striped Beach Dress",
+    productRating: {
+      stars: 4.5,
+      count: 235
+    },
+    productPrice: 2970,
+    keywords: ["robe", "swimsuit", "swimming", "bathing", "apparel"]
+  },
+  {
+    productId: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
+    productImage: "images/products/women-sandal-heels-white-pink.jpg",
+    productName: "Women's Sandal Heels - Pink",
+    productRating: {
+      stars: 4.5,
+      count: 2286
+    },
+    productPrice: 5300,
+    keywords: ["garbage", "bins", "cans", "kitchen"]
+  },
+  {
+    productId: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    productImage: "images/products/round-sunglasses-gold.jpg",
+    productName: "Round Sunglasses",
+    productRating: {
+      stars: 4.5,
+      count: 30
+    },
+    productPrice: 3560,
+    keywords: ["accessories", "shades"]
+  },
+  {
+    productId: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
+    productImage: "images/products/blackout-curtain-set-beige.jpg",
+    productName: "Blackout Curtains Set - Beige",
+    productRating: {
+      stars: 4.5,
+      count: 232
+    },
+    productPrice: 4599,
+    keywords: ["bedroom", "curtains", "home"]
+  },
+  {
+    productId: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
+    productImage: "images/products/women-summer-jean-shorts.jpg",
+    productName: "Women's Summer Jean Shorts",
+    productRating: {
+      stars: 4,
+      count: 160
+    },
+    productPrice: 1699,
+    keywords: ["shorts", "apparel", "mens"]
+  },
+  {
+    productId: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
+    productImage: "images/products/electric-steel-hot-water-kettle-white.jpg",
+    productName: "Electric Hot Water Kettle - White",
+    productRating: {
+      stars: 5,
+      count: 846
+    },
+    productPrice: 5074,
+    keywords: ["water kettle", "appliances", "kitchen"]
+  },
+  {
+    productId: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    productImage: "images/products/knit-athletic-sneakers-gray.jpg",
+    productName: "Waterproof Knit Athletic Sneakers - Gray",
+    productRating: {
+      stars: 4,
+      count: 89
+    },
+    productPrice: 5390,
+    keywords: ["shoes", "running shoes", "footwear"]
+  },
+  {
+    productId: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
+    productImage: "images/products/straw-sunhat.jpg",
+    productName: "Straw Wide Brim Sun Hat",
+    productRating: {
+      stars: 4,
+      count: 215
+    },
+    productPrice: 2200,
+    keywords: ["hats", "straw hats", "summer", "apparel"]
+  },
+  {
+    productId: "1c079479-8586-494f-ab53-219325432536",
+    productImage: "images/products/men-athletic-shoes-white.jpg",
+    productName: "Men's Athletic Sneaker - White",
+    productRating: {
+      stars: 4,
+      count: 229
+    },
+    productPrice: 4590,
+    keywords: ["shoes", "running shoes", "footwear", "mens"]
+  },
+  {
+    productId: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
+    productImage: "images/products/men-stretch-wool-sweater-black.jpg",
+    productName: "Men's Wool Sweater - Black",
+    productRating: {
+      stars: 4.5,
+      count: 2465
+    },
+    productPrice: 3374,
+    keywords: ["sweaters", "apparel"]
+  },
+  {
+    productId: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
+    productImage: "images/products/bathroom-mat.jpg",
+    productName: "Bathroom Bath Mat 16 x 32 Inch - Grey",
+    productRating: {
+      stars: 4.5,
+      count: 119
+    },
+    productPrice: 1850,
+    keywords: ["bathmat", "bathroom", "home"]
+  },
+  {
+    productId: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
+    productImage: "images/products/women-knit-ballet-flat-white.jpg",
+    productName: "Women's Ballet Flat - White",
+    productRating: {
+      stars: 4,
+      count: 326
+    },
+    productPrice: 2640,
+    keywords: ["shoes", "flats", "womens", "footwear"]
+  },
+  {
+    productId: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
+    productImage: "images/products/men-golf-polo-t-shirt-gray.jpg",
+    productName: "Men's Golf Polo Shirt - Gray",
+    productRating: {
+      stars: 4.5,
+      count: 2556
+    },
+    productPrice: 1599,
+    keywords: ["tshirts", "shirts", "apparel", "mens"]
+  },
+  {
+    productId: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    productImage: "images/products/laundry-detergent-tabs.jpg",
+    productName: "Laundry Detergent Tabs, 50 Loads",
+    productRating: {
+      stars: 4.5,
+      count: 305
+    },
+    productPrice: 2899,
+    keywords: ["bathroom", "cleaning"]
+  },
+  {
+    productId: "e4f64a65-1377-42bc-89a5-e572d19252e2",
+    productImage: "images/products/sky-leaf-branch-earrings.jpg",
+    productName: "Sterling Silver Leaf Branch Earrings",
+    productRating: {
+      stars: 4.5,
+      count: 52
+    },
+    productPrice: 6799,
+    keywords: ["jewelry", "accessories", "womens"]
+  },
+  {
+    productId: "19c6a64a-5463-4d45-9af8-e41140a4100c",
+    productImage: "images/products/duvet-cover-set-gray-queen.jpg",
+    productName: "Duvet Cover Set, Diamond Pattern",
+    productRating: {
+      stars: 4,
+      count: 456
+    },
+    productPrice: 4399,
+    keywords: ["bedroom", "bed sheets", "sheets", "covers", "home"]
+  },
+  {
+    productId: "d2785924-743d-49b3-8f03-ec258e640503",
+    productImage: "images/products/women-knit-beanie-pom-pom-blue.jpg",
+    productName: "Women's Knit Winter Beanie - Blue",
+    productRating: {
+      stars: 5,
+      count: 83
+    },
+    productPrice: 1950,
+    keywords: ["hats", "winter hats", "beanies", "apparel", "womens"]
+  },
+  {
+    productId: "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
+    productImage: "images/products/men-chino-pants-beige.jpg",
+    productName: "Men's Chino Pants - Beige",
+    productRating: {
+      stars: 4.5,
+      count: 9017
+    },
+    productPrice: 2290,
+    keywords: ["pants", "apparel", "mens"]
+  },
+  {
+    productId: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
+    productImage: "images/products/men-navigator-sunglasses-black.jpg",
+    productName: "Men's Navigator Sunglasses",
+    productRating: {
+      stars: 3.5,
+      count: 42
+    },
+    productPrice: 3690,
+    keywords: ["sunglasses", "glasses", "accessories", "shades"]
+  },
+  {
+    productId: "04701903-bc79-49c6-bc11-1af7e3651358",
+    productImage: "images/products/men-brown-flat-sneakers.jpg",
+    productName: "Men's Brown Flat Sneakers",
+    productRating: {
+      stars: 4.5,
+      count: 562
+    },
+    productPrice: 2499,
+    keywords: ["footwear", "men", "sneakers"]
+  },
+  {
+    productId: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
+    productImage: "images/products/non-stick-cooking-set-4-pieces.jpg",
+    productName: "Non-Stick Cook Set With Lids - 4 Pieces",
+    productRating: {
+      stars: 4.5,
+      count: 511
+    },
+    productPrice: 6797,
+    keywords: ["cooking set", "kitchen"]
+  },
+  {
+    productId: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
+    productImage: "images/products/vanity-mirror-pink.jpg",
+    productName: "Vanity Mirror with LED Lights - Pink",
+    productRating: {
+      stars: 4.5,
+      count: 130
+    },
+    productPrice: 2549,
+    keywords: ["bathroom", "washroom", "mirrors", "home"]
+  },
+  {
+    productId: "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
+    productImage: "images/products/women-relaxed-lounge-pants-pink.jpg",
+    productName: "Women's Relaxed Lounge Pants - Pink",
+    productRating: {
+      stars: 4.5,
+      count: 248
+    },
+    productPrice: 3400,
+    keywords: ["pants", "pants", "apparel", "womens"]
+  },
+  {
+    productId: "d339adf3-e004-4c20-a120-40e8874c66cb",
+    productImage: "images/products/crystal-zirconia-stud-earrings-pink.jpg",
+    productName: "Crystal Zirconia Stud Earrings - Pink",
+    productRating: {
+      stars: 4.5,
+      count: 117
+    },
+    productPrice: 3467,
+    keywords: ["accessories", "womens"]
+  },
+  {
+    productId: "d37a651a-d501-483b-aae6-a9659b0757a0",
+    productImage: "images/products/glass-screw-lid-food-containers.jpg",
+    productName: "Glass Screw Lid Containers - 3 Pieces",
+    productRating: {
+      stars: 4,
+      count: 126
+    },
+    productPrice: 2899,
+    keywords: ["food containers", "kitchen"]
+  },
+  {
+    productId: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
+    productImage: "images/products/black-and-silver-espresso-maker.jpg",
+    productName: "Black and Silver Espresso Maker",
+    productRating: {
+      stars: 4.5,
+      count: 1211
+    },
+    productPrice: 8250,
+    keywords: ["espresso makers", "kitchen", "appliances"]
+  },
+  {
+    productId: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
+    productImage: "images/products/blackout-curtains-set-teal.jpg",
+    productName: "Blackout Curtains Set 42 x 84-Inch - Teal",
+    productRating: {
+      stars: 4.5,
+      count: 363
+    },
+    productPrice: 3099,
+    keywords: ["bedroom", "home", "curtains"]
+  },
+  {
+    productId: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
+    productImage: "images/products/bath-towel-set-gray-rosewood.jpg",
+    productName: "Bath Towels 2 Pack - Gray, Rosewood",
+    productRating: {
+      stars: 4.5,
+      count: 93
+    },
+    productPrice: 2990,
+    keywords: ["bathroom", "home", "towels"]
+  },
+  {
+    productId: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
+    productImage: "images/products/athletic-skateboard-shoes-gray.jpg",
+    productName: "Athletic Skateboard Shoes - Gray",
+    productRating: {
+      stars: 4,
+      count: 89
+    },
+    productPrice: 3390,
+    keywords: ["shoes", "running shoes", "footwear"]
+  },
+  {
+    productId: "77a845b1-16ed-4eac-bdf9-5b591882113d",
+    productImage: "images/products/countertop-push-blender-black.jpg",
+    productName: "Countertop Push Blender - Black",
+    productRating: {
+      stars: 4,
+      count: 3
+    },
+    productPrice: 10747,
+    keywords: ["food blenders", "kitchen", "appliances"]
+  },
+  {
+    productId: "bc2847e9-5323-403f-b7cf-57fde044a955",
+    productImage: "images/products/men-cozy-fleece-hoodie-light-teal.jpg",
+    productName: "Men's Fleece Hoodie - Light Teal",
+    productRating: {
+      stars: 4.5,
+      count: 3157
+    },
+    productPrice: 3800,
+    keywords: ["sweaters", "hoodies", "apparel", "mens"]
+  },
+  {
+    productId: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
+    productImage: "images/products/artistic-bowl-set-6-piece.jpg",
+    productName: "Artistic Bowl and Plate Set - 6 Pieces",
+    productRating: {
+      stars: 5,
+      count: 679
+    },
+    productPrice: 3899,
+    keywords: ["bowls set", "kitchen"]
+  },
+  {
+    productId: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
+    productImage: "images/products/kitchen-paper-towels-8-pack.jpg",
+    productName: "2-Ply Kitchen Paper Towels - 8 Pack",
+    productRating: {
+      stars: 4.5,
+      count: 1045
+    },
+    productPrice: 1899,
+    keywords: ["kitchen", "kitchen towels", "tissues"]
+  }
+];

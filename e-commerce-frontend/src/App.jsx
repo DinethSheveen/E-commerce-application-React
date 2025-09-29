7824caf05f793f99d9f3ffa7c001b0b88a1959b4
DinethSheveen <fdineth4000@gmail.com>
2025-09-29 17:15:30 +0100

    Passed loadCart function as a prop to Checkout.jsx

--- e-commerce-frontend/src/App.jsx
+++ e-commerce-frontend/src/App.jsx
@@ -1,34 +1,36 @@
-import axios from "axios"
-import {useState,useEffect} from "react"
-import Homepage from './pages/Homepage'
-import {Routes, Route} from 'react-router'
-import Checkout from './pages/Checkout'
-import Orders from './pages/Orders'
-import Tracking from './pages/Tracking'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Homepage from "./pages/Homepage";
+import { Routes, Route } from "react-router";
+import Checkout from "./pages/Checkout";
+import Orders from "./pages/Orders";
+import Tracking from "./pages/Tracking";
 
 function App() {
-  const[cart,setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
-  useEffect(()=>{
-    const loadCart = async()=>{
-      const response = await axios.get("/api/cart-items?expand=product")
-      setCart(response.data);
-    }
+  const loadCart = async () => {
+    const response = await axios.get("/api/cart-items?expand=product");
+    setCart(response.data);
+  };
 
-    loadCart()
-  })
-    
+  useEffect(() => {
+    loadCart();
+  });
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Homepage cartProducts={cart}/>}/>
-      <Route path='checkout' element={<Checkout cartProducts={cart}/>}/>
-      <Route path='orders' element={<Orders cartProducts={cart}/>}/>
-      <Route path='tracking' element={<Tracking cartProducts={cart}/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage cartProducts={cart} />} />
+        <Route
+          path="checkout"
+          element={<Checkout cartProducts={cart} loadCart={loadCart} />}
+        />
+        <Route path="orders" element={<Orders cartProducts={cart} />} />
+        <Route path="tracking" element={<Tracking cartProducts={cart} />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
